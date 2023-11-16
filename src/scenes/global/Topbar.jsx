24463798b56a12a68f4c4d07f457091c08db40f2
Laudier2 <trabalhandooline2019@hotmail.com>
2023-11-15 23:02:26 -0300

    Update

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -6,7 +6,7 @@ import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+//import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import LogoutIcon from '@mui/icons-material/Logout';
 import SearchIcon from "@mui/icons-material/Search";
 const Topbar = () => {
